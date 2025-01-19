f16cfeeea855969014a5c1650ee8562944e8e2b1
darren thiorres <darrenthiores@gmail.com>
2025-01-19 11:42:10 +0700

    Initial Commit

---
+++ ContactGenerator/ContactGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactGeneratorApp.swift
+//  ContactGenerator
+//
+//  Created by Darren Thiores on 19/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ContactGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
