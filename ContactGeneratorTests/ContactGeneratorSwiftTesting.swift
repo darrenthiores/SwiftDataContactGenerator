4cb9b846b0ff69c3e8a97402bbc7cf2abf164ffb
darren thiorres <darrenthiores@gmail.com>
2025-06-25 01:58:48 +0700

    + Added swift Testing

---
+++ ContactGeneratorTests/ContactGeneratorSwiftTesting.swift
@@ -0,0 +1,43 @@
+//
+//  ContactGeneratorSwiftTesting.swift
+//  ContactGeneratorTests
+//
+//  Created by Darren Thiores on 24/06/25.
+//
+
+import Testing
+@testable import ContactGenerator
+
+struct ContactGeneratorSwiftTesting {
+    private var localDataSource: ContactLocalDataSource!
+    private var contactGenerator: ContactGenerator!
+    
+    @MainActor
+    init() {
+        let swiftDataContextManager = MockSwiftDataContextManager()
+        let container = swiftDataContextManager.container
+        let context = swiftDataContextManager.context
+        
+        localDataSource = ContactLocalDataSource(
+            container: container,
+            context: context
+        )
+        contactGenerator = .shared
+    }
+    
+    @Test("Test contact generation, and insert contact into local database") @MainActor
+    func testGenerateThenInsertContact() async throws {
+        var currentContacts: [Contact] = []
+        
+        // Check if contact is empty
+        currentContacts = localDataSource.fetchContacts()
+        #expect(currentContacts == [])
+        
+        let contact = contactGenerator.generateContact()
+        localDataSource.insert(contact)
+        
+        // Check if contact is inserted
+        currentContacts = localDataSource.fetchContacts()
+        #expect(currentContacts.contains(contact))
+    }
+}
