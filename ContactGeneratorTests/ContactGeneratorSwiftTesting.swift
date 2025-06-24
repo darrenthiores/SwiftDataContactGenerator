957a7362bd646b7f602abba129827d7f2aceb748
darren thiorres <darrenthiores@gmail.com>
2025-06-25 02:44:21 +0700

    + Update Swift Testing test case

--- ContactGeneratorTests/ContactGeneratorSwiftTesting.swift
+++ ContactGeneratorTests/ContactGeneratorSwiftTesting.swift
@@ -8,15 +8,16 @@
 import Testing
 @testable import ContactGenerator
 
+@Suite("Contact Generator Tests", .serialized)
 struct ContactGeneratorSwiftTesting {
+    private static let swiftDataContextManager = MockSwiftDataContextManager()
     private var localDataSource: ContactLocalDataSource!
     private var contactGenerator: ContactGenerator!
     
     @MainActor
     init() {
-        let swiftDataContextManager = MockSwiftDataContextManager()
-        let container = swiftDataContextManager.container
-        let context = swiftDataContextManager.context
+        let container = ContactGeneratorSwiftTesting.swiftDataContextManager.container
+        let context = ContactGeneratorSwiftTesting.swiftDataContextManager.context
         
         localDataSource = ContactLocalDataSource(
             container: container,
@@ -39,5 +40,21 @@ struct ContactGeneratorSwiftTesting {
         // Check if contact is inserted
         currentContacts = localDataSource.fetchContacts()
         #expect(currentContacts.contains(contact))
+    }
+    
+    @Test("Test delete contact from local database") @MainActor
+    func testDeleteContact() async throws {
+        var currentContacts: [Contact] = []
+        
+        // Check if contacts is not empty
+        currentContacts = localDataSource.fetchContacts()
+        #expect(currentContacts.count > 0)
+        
+        let contact = try #require(currentContacts.first)
+        localDataSource.delete(contact)
+        
+        // Check if contact is deleted
+        currentContacts = localDataSource.fetchContacts()
+        #expect(currentContacts.count == 0)
     }
 }
